05d7e9f7ac1fb8af17e7890f2069b659720a0ba1
dvorav <d.vorav612@gmail.com>
2020-12-04 01:47:39 -0600

    Files made

---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+
+// fs.readFile("data.csv", 'utf8', (error, data) => error ? console.error(error) : console.log(data))
+
+// fs.writeFile('log.txt', process.argv[2], (err) => err ? console.error(err) : console.log('Success!'));
+
+const inquirer = require("inquirer");
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of the project?",
+  },
+  {
+    type: "input",
+    name: "instructions",
+    message: "What are the installation instructions to the project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the project used for?",
+  },
+
+  {
+    type: "input",
+    name: "contributions",
+    message: "Who are the contributors to the project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What is needed to test the project?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What license was used for the project?",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  console.log(questions.title + "Helo thiere!!!!!!")
+];
+// function writeToFile(filename, data) {
+//   fs.writeFile("./demo/" + filename, data, function (err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("YaY" + filename);
+//   });
+// }
+
+// function init() {
+//     inquirer.prompt(questions).then(function(data) {
+//         writeToFile("DemonREADMe.md")
+//     })
+// }
+// init()
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, "  "));
+});
